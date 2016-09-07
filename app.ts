5d95c23498107d225a0c18a270c2c40b2c257b6c
Jimmy Jones <jimjones26@gmail.com>
2016-09-07 18:31:58 -0400

    stubbed out html and code for submitting a new link

--- app.ts
+++ app.ts
@@ -1,33 +1,45 @@
-/**
-* A basic hello-world Angular 2 app
-*/
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 @Component({
-    selector: 'hello-world',
+    selector: 'reddit',
     template: `
-        <ul>
-            <li *ngFor='let name of names'>Hello {{ name }}</li>
-        </ul>
-        `
-})
-class HelloWorld {
-    names: string[];
+        <form class="ui large form segment">
+            <h3 class="ui header">Add a Link</h3>
 
-    constructor() {
-        this.names = ['Felipe', 'Ari', 'Carlos'];
+            <div class="field">
+                <label for="title">Title:</label>
+                <input name="title" #newtitle>
+            </div>
+            <div class="field">
+                <label for="link">Link:</label>
+                <input name="link" #newlink>
+            </div>
+            <button (click)="addArticle(newtitle, newlink)"  
+            class="ui positive right floated button">  
+                Submit link  
+            </button>
+        </form>
+    `
+})
+class RedditApp {
+    /* TODO: finish out addArticle function */
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+        console.log(`
+            Adding article title: ${ title.value } and link: ${ link.value }
+        `);
+        return false;
     }
 }
 
 @NgModule({
-    declarations: [HelloWorld],
+    declarations: [RedditApp],
     imports: [BrowserModule],
-    bootstrap: [HelloWorld],
+    bootstrap: [RedditApp],
 })
-class HelloWorldAppModule {
+class RedditAppModule {
 
 }
 
-platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
+platformBrowserDynamic().bootstrapModule(RedditAppModule);
