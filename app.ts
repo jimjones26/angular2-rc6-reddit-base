86ba9533b31608437661fe20d27c642d34505f5f
Jimmy Jones <jimjones26@gmail.com>
2016-09-09 00:58:21 -0400

    refactor reddit app list to handle multiple items

--- app.ts
+++ app.ts
@@ -35,6 +35,7 @@ class Article {
 // article component
 @Component({
     selector: 'reddit-article',
+    inputs: ['article'],
     host: {
         class: 'row'
     },
@@ -71,10 +72,6 @@ class Article {
 class ArticleComponent {
     article: Article;
 
-    constructor() {
-        this.article = new Article('Angular 2', 'http://angular.io', 10);
-    }
-
     voteUp() {
         this.article.voteUp();
         return false;
@@ -107,17 +104,21 @@ class ArticleComponent {
             </button>
         </form>
         <div class="ui grid posts">
-            <reddit-article></reddit-article>
+            <reddit-article 
+                *ngFor="let article of articles"
+                [article]="article"></reddit-article>
         </div>
     `
 })
 class RedditApp {
-    // TODO: finish out addArticle function
-    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-        console.log(`
-            Adding article title: ${ title.value} and link: ${link.value}
-        `);
-        return false;
+    articles: Article[];
+
+    constructor() {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io', 3),
+            new Article('Fullstack', 'http://fullstack.io', 2),
+            new Article('Angular Homepage', 'http://angular.io', 1)
+        ];
     }
 }
 
